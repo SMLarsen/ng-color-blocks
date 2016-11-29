1a6361ced6c7d11944fafff20ed22a9bf5720fbe
Steve Larsen <stevelarsen01@gmail.com>
2016-11-28 19:08:50 -0600

    settings can add color

--- server/public/scripts/controllers/settingsController.js
+++ server/public/scripts/controllers/settingsController.js
@@ -1,5 +1,13 @@
-colorBlocks.controller('SettingsController', ['$scope', function($scope) {
+colorBlocks.controller('SettingsController', ['$scope', 'DataFactory', function($scope, DataFactory) {
   console.log("settings controller running");
 
-  
+  var self = this;
+  self.colors = DataFactory.colors;
+  self.colorArray = DataFactory.colorArray;
+  self.newColor = '';
+
+  self.addColor = function() {
+    console.log(self.newColor);
+    DataFactory.colors.push(self.newColor);
+  };
 }]);
