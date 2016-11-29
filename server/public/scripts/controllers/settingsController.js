b68c1eb5e8f482eba8911815f711df93079fd208
Steve Larsen <stevelarsen01@gmail.com>
2016-11-28 20:22:41 -0600

    style config

--- server/public/scripts/controllers/settingsController.js
+++ server/public/scripts/controllers/settingsController.js
@@ -1,23 +1,23 @@
 colorBlocks.controller('SettingsController', ['$scope', 'DataFactory', function($scope, DataFactory) {
-  console.log("settings controller running");
+    console.log("settings controller running");
 
-  var self = this;
-  self.colors = DataFactory.colors;
-  self.colorArray = DataFactory.colorArray;
-  self.newColor = '';
+    var self = this;
+    self.colors = DataFactory.colors;
+    self.colorArray = DataFactory.colorArray;
+    self.newColor = '';
 
-  self.addColor = function() {
-    console.log(self.newColor);
-    DataFactory.addColor(self.newColor);
-  };
+    self.addColor = function() {
+        console.log(self.newColor);
+        DataFactory.addColor(self.newColor);
+    };
 
-  self.removeColor = function(index) {
-    DataFactory.removeColor(index);
-  };
+    self.removeColor = function(index) {
+        DataFactory.removeColor(index);
+    };
 
-  self.changeColor = function(index, color) {
-    console.log('update');
-    DataFactory.changeColor(index, color);
-  };
+    self.changeColor = function(index, color) {
+        console.log('update');
+        DataFactory.changeColor(index, color);
+    };
 
 }]);
--- server/public/scripts/services/dataFactory.js
+++ server/public/scripts/services/dataFactory.js
@@ -1,25 +1,25 @@
 colorBlocks.factory('DataFactory', [function() {
-  console.log("data factory running");
+    console.log("data factory running");
 
-  var colors = ['red', 'blue', 'magenta', 'green', 'pink'];
-  var colorArray = ["AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "DarkOrange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue", "Purple", "RebeccaPurple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue", "SlateGray", "Snow", "SpringGreen", "SteelBlue", "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow",
-  "YellowGreen"
-  ];
+    var colors = ['red', 'blue', 'magenta', 'green', 'pink'];
+    var colorArray = ["AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "DarkOrange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue", "Purple", "RebeccaPurple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue", "SlateGray", "Snow", "SpringGreen", "SteelBlue", "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow",
+        "YellowGreen"
+    ];
 
-  return {
-    colors: colors,
-    colorArray: colorArray,
-    addColor: function(color) {
-      colors.push(color);
-    },
-    removeColor: function(index) {
-      console.log('remove', colors[index]);
-      colors.splice(index,1);
-    },
-    changeColor: function(index, color) {
-      console.log('update', index, color);
-      colors[index] = color;
-    }
-};
+    return {
+        colors: colors,
+        colorArray: colorArray,
+        addColor: function(color) {
+            colors.push(color);
+        },
+        removeColor: function(index) {
+            console.log('remove', colors[index]);
+            colors.splice(index, 1);
+        },
+        changeColor: function(index, color) {
+            console.log('update', index, color);
+            colors[index] = color;
+        }
+    };
 
 }]);
